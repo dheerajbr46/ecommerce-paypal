3a33ac60b1bb8454cdbffdbc6c4dcf5f892198dd
Dheeraj Reddy <33346373+dheerajbr46@users.noreply.github.com>
2025-04-06 22:30:31 -0700

    First Commit

---
+++ src/main/java/com/example/ecommerce_paypal/EcommercePaypalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_paypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePaypalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce_paypal/EcommercePaypalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_paypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePaypalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
